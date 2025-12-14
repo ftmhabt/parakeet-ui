4c7a05ac9deed386908bd699565523e44223a9b1
ftmhabt <ftmh.a78@gmail.com>
2025-12-14 17:36:48 +0330

    Define paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   build: {
     lib: {
       entry: resolve(__dirname, "lib/main.js"),
