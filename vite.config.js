f65d62ae30f91571970716c27c708c41084f5b32
ftmhabt <ftmh.a78@gmail.com>
2025-12-14 17:54:53 +0330

    Build

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+export default defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    build: {
+        lib: {
+            entry: resolve(__dirname, "lib/main.js"),
+            name: "parakeet-ui",
+            fileName: "parakeet-ui",
+        },
+        rollupOptions: {
+            external: ["react", "react-dom", "react/jsx-runtime"],
+            output: {
+                globals: {
+                    react: "React",
+                    "react-dom": "ReactDOM",
+                    "react/jsx-runtime": "react/jsx-runtime",
+                },
+            },
+        },
+    },
+});
